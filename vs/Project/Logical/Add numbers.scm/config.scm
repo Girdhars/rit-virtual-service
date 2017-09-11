9781ac1a5a396cc91cdb7007dab586de8625be1a
Girdhar Singh (148773) <GIRDHARS@amdocs.com>
2017-09-11 13:36:38 +0530

    first commit

---
+++ vs/Project/Logical/Add numbers.scm/config.scm
@@ -0,0 +1,3 @@
+<?xml version="1.0" encoding="UTF-8"?><editableResource createdTimestamp="1505114478327" createdUser="GIRDHARS" id="-798ad0f4:15e6fcc3994:-7f42" type="service_component_resource" updatedTimestamp="1505114478327" updatedUser="GIRDHARS" version="1.1">
+   <loggingConfiguration debug="false" writeStubEventsToDB="false"/>
+</editableResource>
